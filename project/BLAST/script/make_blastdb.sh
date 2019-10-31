625194f9762923301a90a23ae8914f3c6cb8316a
Bin He <bin-he@uiowa.edu>
2019-10-31 11:47:54 -0500

    automate makeblastdb

---
+++ project/BLAST/script/make_blastdb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# 2019-10-31
+# Bin He
+# Script to make blast database
+
+# concatenate the downloaded fasta sequences (it's ok to leave them gzipped) into a single file for makeblastdb
+pushd ../data/genome_seq/GenBank
+cat *[^me]_genomic.fna.gz > all_for_blast_genomic.fna.gz
+cat *rna.fna.gz > all_for_blast_rna.fna.gz
+cat *rna_from_genomic.fna.gz > all_for_blast_rna_from_genomic.fna.gz
+popd
+
+# then go to the blastdb directory and make the blast databases
+pushd ../data/blastdb
+gunzip -c ../genome_seq/GenBank/all_for_blast_genomic.fna.gz | makeblastdb -in - -parse_seqids -dbtype nucl -title all_for_blast_genomic -out all_for_blast_genomic
+gunzip -c ../genome_seq/GenBank/all_for_blast_rna.fna.gz | makeblastdb -in - -parse_seqids -dbtype nucl -title all_for_blast_rna -out all_for_blast_rna
+gunzip -c ../genome_seq/GenBank/all_for_blast_rna_from_genomic.fna.gz | makeblastdb -in - -parse_seqids -dbtype nucl -title all_for_blast_rna_from_genomic -out all_for_blast_rna_from_genomic
+popd
+
+echo "blast databases made."
